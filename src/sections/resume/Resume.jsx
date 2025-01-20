402898a8d2dca68c02ba7b370be3442c5760bd45
Chinaza Goodness <111002789+Nazasophie@users.noreply.github.com>
2025-01-20 15:26:41 +0100

    Fashion

--- src/sections/resume/Resume.jsx
+++ src/sections/resume/Resume.jsx
@@ -12,115 +12,85 @@ const Resume = ({ title, subtitle, note }) => {
                 <div className="line-divider"></div>
 
                 <div className="resume-item">
-                    <div className="item-header">
-                        <h3 className="item-title">Junior Developer & Designer</h3>
-                        <p className="item-date">December 2022 - September 2023 </p>
-                    </div>
+                    
                     <div className="item-content">
-                        <h3 className="item-title">Dawih Solutions (Remote)</h3>
-                        <p className="item-text">
-                            Design, develop, and maintain fully-fledged and functioning websites 
-                            and other platforms with databases, working on both front-end and 
-                            back-end development processes. 
-                        </p>
-                        <p className="item-text">
-                            I Provide professional web designs and solutions as well as
-                            provided support and maintance for wordpress solutions.
-                        </p>
+                        <h3 className="item-title">African Leadership Software Engineering Accelerator (Remote)</h3>
+                        <p className="item-date">September 2022 - October 2023 </p>
+                        <ol className="item-text">
+                            <li>Completed a 12-month program focusing on software engineering  fundamentals </li>
+                            <li>Built a replica of the Airbnb website using Python's Flask Web Framework and deployed it to a
+                                server</li>. 
+                            <li>Collaborated as a front-end developer in designing and developing the "Space Tech" blog
+                             website during the final year, contributing significantly to the user interface and user
+                               experience.</li>
+                        </ol>
+                        
                     </div>
                 </div>
                 <div className="resume-item">
                     <div className="item-header">
-                        <h3 className="item-title">Web Developer (Intern)</h3>
-                        <p className="item-date">August 2019 - March 2020 </p>
+                        <h3 className="item-title">React Developer (Virtual Intern)</h3>
+                        <p className="item-date">01/December 2023 - 31/December 2023 </p>
                     </div>
                     <div className="item-content">
-                        <h3 className="item-title">Xtech Global Services</h3>
-                        <p className="item-text">
-                            Assisted with coding under the direction of senior web developers, 
-                            tested existing code, formatted graphics for web use, communicated 
-                            with clients about the project.
-                        </p>
-                        <p className="item-text">
-                            Responsible for troubleshooting website issues and testing website 
-                            functionality as well as new technology for potential use.
-                            I brainstormed novel concepts with project managers
-                            and the production team.
-                        </p>
+                        <h3 className="item-title">Sync Internship</h3>
+                        <ul className="item-text">
+                            <li>Developed multiple applications addressing real-life challenges within a month-long
+                                internsip.</li>
+                        <li>Leveraged API integration and React.js to create a weather application, chat
+                             application, code editor, and resume generator</li>
+                        <li>Demonstrated proficiency in web development and problem-solving skills, delivering
+                             innovative and functional solutions within tight deadlines.</li>
+                        </ul>
+                        
                     </div>
                 </div>
                 <div className="resume-item">
                     <div className="item-header">
-                        <h3 className="item-title">Software Developer (Intern)</h3>
-                        <p className="item-date">April 2017 - August 2018</p>
+                        <h3 className="item-title">Frontend Developer (Intern)</h3>
+                        <p className="item-date">Agust 2023 - November 2023</p>
                     </div>
                     <div className="item-content">
-                        <h3 className="item-title">Zeo Info Tech, Makurdi</h3>
-                        <p className="item-text">
-                            Developed basic applications using, WordPress,  HTML, CSS, PHP, JavaScript, Bootstrap, React, Laravel and codeIgniter.  Helped development, QA, and user experience teams create new products.
-                            Improved software engineering processes and best practices were learned.
-                        </p>
-                        <p className="item-text">
-                            Collaborated with engineers to find and eliminate software bugs.
-                            New applications were tested for usefulness and adherence to the code plan.
-                        </p>
-                        <p className="item-text">
-                            To keep the section operating smoothly, I handled clerical tasks for web
-                            development employees.
-                        </p>
+                        <h3 className="item-title">Enovolem Virtual</h3>
+                        <ul className="item-text">
+                           <li>Collaborated with Enovolem to develop an e-commerce gift store website</li>
+                           <li>Worked as a frontend developer, focusing on creating the user interface</li>
+                           <li>Implemented design elements and features to enhance the website's appeal and
+                            functionality.</li>
+                            <li>Ensured user-friendly navigation and seamless browsing experience for customers.</li>
+                            <li>Utilized HTML, CSS, and React.js to build responsive and visually appealing web pages.</li>
+                        </ul>
+                        
                     </div>
                 </div>
 
             </div>
 
-            <h3 className="container-title">education history<FaGraduationCap className="icon" /></h3>
+            <h3 className="container-title">PROFESSIONAL TRAINING AND CERTIFICATIONS<FaGraduationCap className="icon" /></h3>
             <div className="resume-container">
                 {/* the education section  */}
                 <div className="line-divider"></div>
 
                 <div className="resume-item">
                     <div className="item-header">
-                        <h3 className="item-title">Bachelor Degree</h3>
-                        <p className="item-date">July 2015 - July 2019 </p>
+                        <h3 className="item-title">BSC in Fashion Technology | Yaba College of Technology</h3>
+                        <p className="item-date">In view </p>
                     </div>
                     <div className="item-content">
-                        <h3 className="item-title">Yaba College of Technology</h3>
-                        <p className="item-text">
-                        At Yaba College of Technology, Lagos, my fashion design degree covered an extensive array of modules, 
-                        including Fashion Illustration, Garment Construction, Textile Science, and Pattern Making.
-                        </p>
-                        <p className="item-text">
-                        I also honed skills in Fashion Marketing, Sustainable Design,
-                         Fashion Technology, Trend Forecasting, and the Business of Fashion, ensuring a well-rounded foundation for a career in the fashion industry
-                        </p>
-                    </div>
-                </div>
-                <div className="resume-item">
-                    <div className="item-header">
-                        <h3 className="item-title"> High School</h3>
-                        <p className="item-date"> June 2004 - July 2010 </p>
+                        <h3 className="item-title">Front-end developer | Women in Tech</h3>
+                 
                     </div>
                     <div className="item-content">
-                        <h3 className="item-title">West Of Mines Jos</h3>
-                        <p className="item-text">
-                            I obtained my high school education here. 
-                            I was equipped with the requisites of a Science student, 
-                            with basic knowledge in mathematics, physics, chemistry, and biology.
-                        </p>
-                    </div>
-                </div>
-                <div className="resume-item">
-                    <div className="item-header">
-                        <h3 className="item-title"> Primary Education</h3>
-                        <p className="item-date"> June 1999 - July 2004 </p>
+                        <h3 className="item-title">Software Engineer (Certified) | ALX</h3>
+                 
                     </div>
                     <div className="item-content">
-                        <h3 className="item-title">Police Children School Jos</h3>
-                        <p className="item-text">
-                            I obtanined my first school leaving certificate.
-                        </p>
+                        <h3 className="item-title">UIX Designer | Digi girls</h3>
+                 
                     </div>
                 </div>
+               
+                 
             </div>
 
 
